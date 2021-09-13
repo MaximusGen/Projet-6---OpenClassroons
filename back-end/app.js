910d9d846db49e6f3722c3830d477a7aef8eeeaa
MaximusGen <maxime_paupy@hotmail.fr>
2021-09-13 15:34:28 +0200

    first commit

---
+++ back-end/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://maximusgen:Drakaxx92@test.4wl0j.mongodb.net/sauce?retryWrites=true&w=1",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const bodyParser = require("body-parser");
+
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("api/auth", userRoutes);
+
+module.exports = app;
